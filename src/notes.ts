78bfc3e2704641030ce512d0ad0e6a50913ae13f
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2025-05-17 18:10:46 -0700

    Added create note functionality

--- src/notes.ts
+++ src/notes.ts
@@ -1,9 +1,54 @@
-const createNoteForm = document.getElementById("create-note-form");
+const getNotesForm = <HTMLFormElement>document.getElementById("get-notes-form");
+const noteByIdForm = <HTMLFormElement>(
+    document.getElementById("note-by-id-form")
+);
+const createNoteForm = <HTMLFormElement>(
+    document.getElementById("create-note-form")
+);
 
-createNoteForm?.addEventListener("submit", async (e) => {
+getNotesForm.addEventListener("submit", async (e) => {
     e.preventDefault();
     try {
         const response = await fetch("http://127.0.0.1:3000/api/v1/notes");
+        const data = await response.json();
+        if (!data) throw new Error("No data received");
+        console.log(data);
+    } catch (error) {
+        if (error instanceof Error) {
+            console.log(error.message);
+        }
+    }
+});
+
+noteByIdForm.addEventListener("submit", async (e) => {
+    try {
+        console.log(e);
+    } catch (error) {
+        if (error instanceof Error) {
+            console.log(error.message);
+        }
+    }
+});
+
+createNoteForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    try {
+        const formData = new FormData(createNoteForm);
+        const title = formData.get("title");
+        const content = formData.get("content");
+        const response = await fetch(
+            "http://127.0.0.1:3000/api/v1/notes/create",
+            {
+                method: "POST",
+                body: JSON.stringify({
+                    title: title,
+                    content: content,
+                }),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        );
         const data = await response.json();
         if (!data) throw new Error("No data received");
         console.log(data);
